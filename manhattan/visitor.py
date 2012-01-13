04bd92c27a4ba39a24aa6cc0638d19ff5a243cc1
Scott Torborg <scott@cartlogic.com>
2012-01-13 14:15:23 -0800

    added some debug logging to Visitor

--- manhattan/visitor.py
+++ manhattan/visitor.py
@@ -1,6 +1,10 @@
+import logging
+
 import time
 
 from .util import choose_population
+
+log = logging.getLogger(__name__)
 
 
 class Visitor(object):
@@ -15,15 +19,19 @@ class Visitor(object):
         return str(int(time.time()))
 
     def page(self, request):
+        log.debug('page: %s %s', self.id, request.url)
         self.log.write(['page', self.timestamp(), self.id, request.url])
 
     def pixel(self):
+        log.debug('pixel: %s', self.id)
         self.log.write(['pixel', self.timestamp(), self.id])
 
     def goal(self, name, value=None):
+        log.debug('goal: %s %s', self.id, name)
         self.log.write(['goal', self.timestamp(), self.id, name, str(value)])
 
     def split(self, test_name, populations=None):
+        log.debug('split: %s %s', self.id, test_name)
         selected = choose_population(self.id + test_name, populations)
         self.log.write(['split', self.timestamp(), self.id, test_name,
                         selected])
