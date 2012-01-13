b230f89adf41775adc4e504312bcadb184d7f085
Scott Torborg <scott@cartlogic.com>
2012-01-13 13:45:59 -0800

    let worker process to multiple backends

--- manhattan/worker.py
+++ manhattan/worker.py
@@ -5,14 +5,15 @@ log = logging.getLogger(__name__)
 
 class Worker(object):
 
-    def __init__(self, log, backend):
+    def __init__(self, log, *backends):
         self.log = log
-        self.backend = backend
+        self.backends = backends
 
     def handle_record(self, record):
-        cmd = record[0]
-        f = getattr(self.backend, 'record_%s' % cmd)
-        f(*record[1:])
+        func_name = 'record_%s' % record[0]
+        for backend in self.backends:
+            f = getattr(backend, func_name)
+            f(*record[1:])
 
     def run(self):
         log.info('Worker started processing.')
