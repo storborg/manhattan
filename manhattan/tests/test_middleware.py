765a4736ccf854faff7e10e8ab2f0ed8281f88d1
Scott Torborg <scott@cartlogic.com>
2012-01-13 15:47:40 -0800

    more thorough tests for middleware pixel injection and handling functionality

--- manhattan/tests/test_middleware.py
+++ manhattan/tests/test_middleware.py
@@ -13,9 +13,16 @@ class SampleApp(object):
 
     def __call__(self, environ, start_response):
         req = Request(environ)
-        s = '<html><body><h1>Hello %s</h1></body></html>' % req.path_info
-        resp = Response(s)
-        resp.content_type = 'text/html'
+
+        if req.path_info.endswith('.txt'):
+            s = 'Hello %s' % req.path_info
+            resp = Response(s)
+            resp.content_type = 'text/plain'
+        else:
+            s = '<html><body><h1>Hello %s</h1></body></html>' % req.path_info
+            resp = Response(s)
+            resp.content_type = 'text/html'
+
         return resp(environ, start_response)
 
 
@@ -27,6 +34,9 @@ app = TestApp(app)
 
 
 class TestMiddleware(TestCase):
+
+    def setUp(self):
+        app.reset()
 
     def test_request(self):
         log.purge()
@@ -46,3 +56,19 @@ class TestMiddleware(TestCase):
         self.assertEqual(len(records), 1)
         record = records[0]
         self.assertEqual(record[0], 'pixel')
+
+        resp = app.get('/foo')
+
+        records = list(log.process())
+        self.assertEqual(len(records), 1)
+        record = records[0]
+        self.assertEqual(record[0], 'page')
+        self.assertTrue(record[3].endswith('/foo'))
+
+    def test_pixel_req(self):
+        resp = app.get('/vpixel.gif')
+        self.assertEqual(resp.content_type, 'image/gif')
+
+    def test_non_html_pixel(self):
+        resp = app.get('/non-html-page.txt')
+        self.assertNotIn('/vpixel.gif', resp.body)
