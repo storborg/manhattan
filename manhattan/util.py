bc5b00355094b6ceb0c29ca8558673a627e212ca
Scott Torborg <scott@cartlogic.com>
2012-02-09 15:41:40 -0800

    fleshed out docstrings in manhattan.util

--- manhattan/util.py
+++ manhattan/util.py
@@ -3,6 +3,9 @@ import random
 import bisect
 
 
+"""
+A 1 pixel transparent GIF as a bytestring.
+"""
 transparent_pixel = (
     'GIF89a'
     '\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04'
@@ -11,42 +14,102 @@ transparent_pixel = (
 
 
 def pixel_tag(path):
+    """
+    Build the HTML surrounding a given image path that will be injected as a
+    tracking pixel.
+
+    :param path:
+        URL path to the tracking pixel.
+    :type request:
+        string
+    :returns:
+        HTML markup for image tag.
+    :rtype:
+        string
+    """
     return '<img src="%s" alt="" />' % path
 
 
 def nonce():
     """
-    Return a random nonce, 160 bits long as a hex string.
+    Build a cryptographically random nonce.
+
+    :returns:
+        Hex string, with 20 bytes (40 hex chars).
+    :rtype:
+        string
     """
     return os.urandom(20).encode('hex')
 
 
-def nonrandom_choice(s, seq):
+def nonrandom_choice(seed, seq):
     """
-    Pick an element from ``seq`` according to the value of string ``s``.
-    Guaranteed to be deterministic, tries to be uniform.
+    Pick an element from the specified sequence ``seq`` based on the value of
+    the specified string ``seed``. Guaranteed to be deterministic, tries to be
+    uniform.
+
+    :param seed:
+        Any string specifier to control the element.
+    :type seed:
+        string
+    :param seq:
+        Python sequence to pick from.
+    :type seq:
+        sequence object
+    :returns:
+        An element from ``seq``.
     """
-    return random.Random(s).choice(seq)
+    return random.Random(seed).choice(seq)
 
 
-def nonrandom(s, n):
+def nonrandom(seed, n):
     """
-    Return a deterministic but pseudo-random number between 0 and ``n``.
+    Return a deterministic but pseudo-random number between 0 and ``n``, based
+    on the value of ``seed``. Guaranteed to be deterministic, tries to be
+    uniform.
+
+    :param seed:
+        Any string specifier to control the output.
+    :type seed:
+        string
+    :param n:
+        Range of output value.
+    :type n:
+        float
+    :returns:
+        Float between 0 and ``n``.
+    :rtype:
+        float
     """
-    return random.Random(s).random() * n
+    return random.Random(seed).random() * n
 
 
-def choose_population(s, populations=None):
+def choose_population(seed, populations=None):
     """
     Randomly pick an element from populations according to type.
+
+    :param seed:
+        Any string specifier to control the output.
+    :type seed:
+        string
+    :param populations:
+        If not specified, perform a straight AB test between True and False. If
+        specified as a sequence, pick uniformly from the sequence. If specified
+        as a dict, use the keys as population names and the values as the
+        weight that each population should receive, and distribute between them
+        according to weight.
+    :type populations:
+        None, sequence, or dict
+    :returns:
+        Selected population
     """
     # Uniform distribution between True, False
     if populations is None:
-        return nonrandom_choice(s, (True, False))
+        return nonrandom_choice(seed, (True, False))
 
     # Uniform distribution over populations
     if isinstance(populations, list):
-        return nonrandom_choice(s, populations)
+        return nonrandom_choice(seed, populations)
 
     # Weighted distribution over populations
     if isinstance(populations, dict):
@@ -62,7 +125,7 @@ def choose_population(s, populations=None):
         if running_mass == 0:
             raise ValueError("Need at least one option with probability > 0")
 
-        r = nonrandom(s, running_mass)
+        r = nonrandom(seed, running_mass)
         i = bisect.bisect(pop_mass, r) - 1
         return pop_name[i]
 
