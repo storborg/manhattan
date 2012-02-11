e76a7e853977af66b34553e8cb0dda2f3c3f9853
Scott Torborg <scott@cartlogic.com>
2012-02-10 21:41:53 -0800

    need to wait a little bit before killing child threads in tests--otherwise the thread may not have set the .is_alive property yet

--- manhattan/tests/test_timerotating_log.py
+++ manhattan/tests/test_timerotating_log.py
@@ -128,6 +128,7 @@ class TimeRotatingLogTest(TestCase):
         log_r = TimeRotatingLog('/tmp/manhattan-test-trl-stayalive-none')
         log_r.sleep_delay = 0.001
         consumed, consumer = make_thread_consumer(log_r)
+        time.sleep(log_r.sleep_delay)
         log_r.is_alive = False
 
     def test_unicode_names(self):
