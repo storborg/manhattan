d5e3ef94d5c9469c3370420cfe6757d01a7c74b0
Scott Torborg <scott@cartlogic.com>
2012-02-11 16:30:10 -0800

    add fake timestamps to test data to use for time range testing

--- manhattan/tests/data.py
+++ manhattan/tests/data.py
@@ -1,3 +1,6 @@
+import types
+from random import randint
+
 from webob import Request
 
 from manhattan import visitor
@@ -5,72 +8,72 @@ from manhattan.visitor import Visitor
 
 
 test_clickstream = [
-    ('page', 'a', '/'),
-    ('page', 'b', '/cheese'),
-    ('pixel', 'a'),
-    ('page', 'a', '/potatoes'),
-    ('page', 'a', '/potatoes/sweet'),
-    ('pixel', 'b'),
-    ('page', 'a', '/potatoes/russet'),
-    ('page', 'b', '/cheese/parmesan'),
-    ('page', 'c', '/fruit/apples'),
-    ('page', 'b', '/cart'),
-    ('goal', 'b', 'add to cart', ''),
-    ('page', 'f', '/'),
-    ('pixel', 'f'),
-    ('page', 'c', '/fruit/bananas'),
-    ('split', 'b', 'red checkout form'),
-    ('page', 'b', '/checkout'),
-    ('goal', 'b', 'began checkout', ''),
-    ('page', 'f', '/fruit'),
-    ('page', 'a', '/cart'),
-    ('goal', 'a', 'add to cart', ''),
-    ('pixel', 'c'),
-    ('page', 'a', '/cheese/gouda'),
-    ('page', 'a', '/cheese'),
-    ('page', 'b', '/cheese'),
-    ('page', 'd', '/'),
-    ('pixel', 'd'),
-    ('split', 'b', 'red checkout form'),
-    ('page', 'd', '/cheese'),
-    ('page', 'd', '/cheese/gruyere'),
-    ('page', 'b', '/checkout/complete'),
-    ('goal', 'b', 'completed checkout', 31.78),
-    ('goal', 'b', 'order margin', 22.5),
-    ('goal', 'b', 'margin per session', 7.15),
-    ('page', 'c', '/'),
-    ('page', 'd', '/cart'),
-    ('goal', 'd', 'add to cart', ''),
-    ('page', 'd', '/checkout'),
-    ('goal', 'd', 'began checkout', ''),
-    ('page', 'a', '/account'),
-    ('page', 'e', '/fruit'),
-    ('pixel', 'e'),
-    ('page', 'd', '/checkout/complete'),
-    ('goal', 'd', 'completed checkout', 64.99),
-    ('goal', 'd', 'order margin', 20.1),
-    ('goal', 'd', 'margin per session', 13.06),
-    ('page', 'e', '/fruit/pineapples'),
-    ('page', 'e', '/fruit/cherries'),
-    ('page', 'e', '/fruit/pears'),
-    ('page', 'e', '/cart'),
-    ('goal', 'e', 'add to cart', ''),
-    ('page', 'e', '/checkout'),
-    ('goal', 'e', 'began checkout', ''),
-    ('page', 'd', '/account'),
-    ('page', 'd', '/'),
-    ('page', 'e', '/checkout/complete'),
-    ('goal', 'e', 'completed checkout', 11.42),
-    ('goal', 'e', 'order margin', 27.8),
-    ('goal', 'e', 'margin per session', 3.17),
-    ('page', 'b', '/fruit'),
-    ('page', 'f', '/cheese'),
-    ('page', 'f', '/cheese/cheddar'),
-    ('page', 'f', '/cart'),
-    ('goal', 'f', 'add to cart', ''),
-    ('page', 'f', '/cheese'),
-    ('page', 'f', '/checkout'),
-    ('goal', 'f', 'began checkout', ''),
+    (10, 'page', 'a', '/'),
+    (10, 'page', 'b', '/cheese'),
+    (132, 'pixel', 'a'),
+    (240, 'page', 'a', '/potatoes'),
+    (290, 'page', 'a', '/potatoes/sweet'),
+    (300, 'pixel', 'b'),
+    (382, 'page', 'a', '/potatoes/russet'),
+    (394, 'page', 'b', '/cheese/parmesan'),
+    (448, 'page', 'c', '/fruit/apples'),
+    (462, 'page', 'b', '/cart'),
+    (1120, 'goal', 'b', 'add to cart', ''),
+    (1200, 'page', 'f', '/'),
+    (1202, 'pixel', 'f'),
+    (1596, 'page', 'c', '/fruit/bananas'),
+    (1602, 'split', 'b', 'red checkout form'),
+    (1602, 'page', 'b', '/checkout'),
+    (1602, 'goal', 'b', 'began checkout', ''),
+    (1706, 'page', 'f', '/fruit'),
+    (1996, 'page', 'a', '/cart'),
+    (1996, 'goal', 'a', 'add to cart', ''),
+    (2112, 'pixel', 'c'),
+    (2196, 'page', 'a', '/cheese/gouda'),
+    (2356, 'page', 'a', '/cheese'),
+    (2680, 'page', 'b', '/cheese'),
+    (2840, 'page', 'd', '/'),
+    (2846, 'pixel', 'd'),
+    (3110, 'split', 'b', 'red checkout form'),
+    (3340, 'page', 'd', '/cheese'),
+    (3514, 'page', 'd', '/cheese/gruyere'),
+    (3514, 'page', 'b', '/checkout/complete'),
+    (3514, 'goal', 'b', 'completed checkout', 31.78),
+    (3514, 'goal', 'b', 'order margin', 22.5),
+    (3514, 'goal', 'b', 'margin per session', 7.15),
+    (3600, 'page', 'c', '/'),
+    (3620, 'page', 'd', '/cart'),
+    (4114, 'goal', 'd', 'add to cart', ''),
+    (4278, 'page', 'd', '/checkout'),
+    (4278, 'goal', 'd', 'began checkout', ''),
+    (4534, 'page', 'a', '/account'),
+    (4600, 'page', 'e', '/fruit'),
+    (4616, 'pixel', 'e'),
+    (4990, 'page', 'd', '/checkout/complete'),
+    (4990, 'goal', 'd', 'completed checkout', 64.99),
+    (4990, 'goal', 'd', 'order margin', 20.1),
+    (4990, 'goal', 'd', 'margin per session', 13.06),
+    (5002, 'page', 'e', '/fruit/pineapples'),
+    (5174, 'page', 'e', '/fruit/cherries'),
+    (5226, 'page', 'e', '/fruit/pears'),
+    (5244, 'page', 'e', '/cart'),
+    (5244, 'goal', 'e', 'add to cart', ''),
+    (5950, 'page', 'e', '/checkout'),
+    (5950, 'goal', 'e', 'began checkout', ''),
+    (6278, 'page', 'd', '/account'),
+    (6396, 'page', 'd', '/'),
+    (6620, 'page', 'e', '/checkout/complete'),
+    (6620, 'goal', 'e', 'completed checkout', 11.42),
+    (6620, 'goal', 'e', 'order margin', 27.8),
+    (6620, 'goal', 'e', 'margin per session', 3.17),
+    (6988, 'page', 'b', '/fruit'),
+    (7020, 'page', 'f', '/cheese'),
+    (7042, 'page', 'f', '/cheese/cheddar'),
+    (7068, 'page', 'f', '/cart'),
+    (7068, 'goal', 'f', 'add to cart', ''),
+    (7198, 'page', 'f', '/cheese'),
+    (7246, 'page', 'f', '/checkout'),
+    (7246, 'goal', 'f', 'began checkout', '')
 ]
 
 
@@ -87,6 +90,8 @@ def run_clickstream(log):
         'margin per session': visitor.CURRENCY,
     }
 
+    start_timestamp = 1329006369
+
     visitors = {}
 
     def get_visitor(vid):
@@ -94,10 +99,18 @@ def run_clickstream(log):
             visitors[vid] = Visitor(vid, log)
         return visitors[vid]
 
+    def set_fake_timestamp(v, ts):
+        def fake_timestamp(self):
+            return '%d.%04d' % (ts, randint(0, 9999))
+        v.timestamp = types.MethodType(fake_timestamp, v, Visitor)
+
     for action in test_clickstream:
-        cmd = action[0]
-        v = get_visitor(action[1])
-        args = action[2:]
+        ts = start_timestamp + action[0]
+        cmd = action[1]
+        v = get_visitor(action[2])
+        args = action[3:]
+
+        set_fake_timestamp(v, ts)
 
         if cmd == 'page':
             req = Request.blank(args[0])
