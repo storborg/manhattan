8db4fc903ce48d59c126cf58091f2c078c2bf6c5
Scott Torborg <scott@cartlogic.com>
2012-02-16 20:06:37 -0800

    add a moderation queue of sorts for visitor ids which are suspected bots. for now it's slightly different in sql and memory backends, but these should be consolidated in the future.

--- manhattan/backends/memory.py
+++ manhattan/backends/memory.py
@@ -5,6 +5,8 @@ class MemoryBackend(object):
 
     def __init__(self):
         self._nonbot = set()
+        self._nonbot_queue = defaultdict(list)
+
         self._visitors = {}
         self._goals = defaultdict(set)
         self._vids_by_variant = defaultdict(set)
@@ -13,7 +15,21 @@ class MemoryBackend(object):
         self._ptr = None
 
     def handle(self, rec, ptr):
-        assert rec.key in ('page', 'pixel', 'goal', 'split')
+        if rec.key == 'pixel':
+            self._nonbot.add(rec.vid)
+            for rec in self._nonbot_queue.pop(rec.vid, ()):
+                self.handle_nonbot(rec)
+
+        elif rec.vid in self._nonbot:
+            self.handle_nonbot(rec)
+
+        else:
+            self._nonbot_queue[rec.vid].append(rec)
+
+        self._ptr = ptr
+
+    def handle_nonbot(self, rec):
+        assert rec.key in ('page', 'goal', 'split')
 
         if rec.key == 'page':
             self._goals['viewed page'].add(rec.vid)
@@ -22,9 +38,6 @@ class MemoryBackend(object):
                                            user_agent=rec.user_agent)
             self._all.add(rec.vid)
 
-        elif rec.key == 'pixel':
-            self._nonbot.add(rec.vid)
-
         elif rec.key == 'goal':
             self._goals[rec.name].add(rec.vid)
 
@@ -32,8 +45,6 @@ class MemoryBackend(object):
             variant = rec.test_name, rec.selected
             self._vids_by_variant[variant].add(rec.vid)
             self._variants_by_vid[rec.vid].add(variant)
-
-        self._ptr = ptr
 
     def get_pointer(self):
         return self._ptr
--- manhattan/backends/sql/__init__.py
+++ manhattan/backends/sql/__init__.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from sqlalchemy import create_engine
 
 from . import model, handling, reporting
@@ -8,14 +9,32 @@ class SQLBackend(object):
 
     def __init__(self, sqlalchemy_url, pool_recycle=3600,
                  max_recent_visitors=500):
+        self._nonbot = set()
+        self._nonbot_queue = defaultdict(list)
+
         self.engine = create_engine(sqlalchemy_url, pool_recycle=pool_recycle)
         handling.max_recent_visitors = max_recent_visitors
         model.init_model(self.engine)
         meta.metadata.create_all()
 
-    def handle(self, record, pointer):
-        handling.handle_record(record)
+    def handle(self, rec, pointer):
+        if rec.key == 'pixel':
+            self.handle_nonbot(rec)
+            self._nonbot.add(rec.vid)
+            for queued_rec in self._nonbot_queue.pop(rec.vid, ()):
+                self.handle_nonbot(queued_rec)
+
+        elif rec.vid in self._nonbot:
+            self.handle_nonbot(rec)
+
+        else:
+            self._nonbot_queue[rec.vid].append(rec)
+
         resuming.update_pointer(pointer)
+        meta.Session.commit()
+
+    def handle_nonbot(self, record):
+        handling.handle_record(record)
         meta.Session.commit()
 
     def get_pointer(self):
