8957ad97b560251e02c857683a5b77301ee49293
Scott Torborg <scott@cartlogic.com>
2012-02-18 13:37:53 -0800

    add performance test for a few client server queries

---
+++ manhattan/tests/perf/test_clientserver.py
@@ -0,0 +1,31 @@
+import time
+
+from manhattan.client import Client
+
+
+client = Client()
+
+
+def test(f, trials=500):
+    start = time.time()
+    for ii in range(trials):
+        f()
+    end = time.time()
+    elapsed = end - start
+    print ("Ran %d trials, %0.2f ms each" %
+           (trials, ((1000. * elapsed) / trials)))
+
+
+def get_results():
+    client.test_results(u'Discount Rate for Datrek')
+
+
+def get_tests():
+    client.tests()
+
+
+if __name__ == '__main__':
+    print "Testing tests list."
+    test(get_tests)
+    print "Testing resuls page."
+    test(get_results)
